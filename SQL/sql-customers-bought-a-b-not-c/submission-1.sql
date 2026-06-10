73098ce8f6dd1d5caccf6e91d5897e948682223f
tropicles <147875053+tropicles@users.noreply.github.com>
2026-06-10 21:41:53 +0530

    Bulk sync: 1 submission

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-1.sql
@@ -0,0 +1,39 @@
+-- Write your query below
+
+select *
+from customers
+where customer_id IN
+(
+select customer_id
+FROM orders
+where customer_id IN
+(
+select *
+from
+(
+select customer_id
+FROM orders
+where ((product_name='A'))
+
+intersect 
+
+select customer_id
+FROM orders
+where ((product_name='B'))
+
+intersect 
+
+select  customer_id
+FROM orders
+where customer_id NOT IN (select customer_id
+from orders
+where product_name='C'
+)
+
+) t
+) 
+group by customer_id
+)
+order by customer_name
+
+
